a82b5d4aff275f541eba5f5834aeffd2b7d2afe0
qamarali <qamarali@qamaralis-MacBook-Pro.local>
2017-07-29 22:52:43 -0500

    inital commit

---
+++ index.ios.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text,View } from 'react-native';
+import Header from "./src/component/header";
+import AlbumList from  "./src/component/AlbumList";
+
+
+const myApp = () => {
+
+    return(
+        <View>
+        <Header headerText= { 'Albums!'} />
+        <AlbumList/>
+        </View>
+    )
+}
+
+
+
+AppRegistry.registerComponent('myApp', () => myApp);
